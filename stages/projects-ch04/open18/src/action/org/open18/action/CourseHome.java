f092922711dcd96840663210fc73220efa8e00d6
dan.j.allen <dan.j.allen@cc8609e4-7d3f-0410-87dc-e7b0ede3ef86>
2007-12-14 22:42:38 +0000

    upgrade to jboss-seam-2.0.0.CR1 merge in changes from chapter 3

--- stages/projects-ch04/open18/src/action/org/open18/action/CourseHome.java
+++ stages/projects-ch04/open18/src/action/org/open18/action/CourseHome.java
@@ -5,9 +5,13 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.faces.context.FacesContext;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.framework.EntityHome;
+import org.jboss.seam.framework.EntityNotFoundException;
 
 @Name("courseHome")
 public class CourseHome extends EntityHome<Course> {
@@ -47,6 +51,29 @@ public class CourseHome extends EntityHome<Course> {
 	}
 
 	/**
+	 * <p>Order the holes according to the value of the number property.</p>
+	 * <p>Note that this could also be done by adding an @OrderBy("number asc") above
+	 * the getHoles() method on the Course entity so that the collection is sorted
+	 * coming out of the database.</p>
+	 */
+	public List<Hole> getHoles() {
+		if (getInstance() == null) {
+			return null;
+		}
+
+		List<Hole> holes =
+				new ArrayList<Hole>(getInstance().getHoles());
+		Collections.sort(holes, new Comparator<Hole>() {
+
+			public int compare(Hole a, Hole b) {
+				return Integer.valueOf(a.getNumber()).compareTo(Integer.valueOf(b.getNumber()));
+			}
+		});
+
+		return holes;
+	}
+
+	/**
 	 * <p>Order the tee sets according to the value of the position property.</p>
 	 * <p>Note that this could also be done by adding an @OrderBy("position asc") above
 	 * the getTeeSets() method on the Course entity so that the collection is sorted
@@ -69,28 +96,34 @@ public class CourseHome extends EntityHome<Course> {
 
 		return teeSets;
 	}
-
-	/**
-	 * <p>Order the holes according to the value of the number property.</p>
-	 * <p>Note that this could also be done by adding an @OrderBy("number asc") above
-	 * the getHoles() method on the Course entity so that the collection is sorted
-	 * coming out of the database.</p>
-	 */
-	public List<Hole> getHoles() {
-		if (getInstance() == null) {
-			return null;
+	
+	public String validateEntityFound() {
+		try {
+			this.getInstance();
+		} catch (EntityNotFoundException e) {
+			return "invalid";
 		}
 
-		List<Hole> holes =
-				new ArrayList<Hole>(getInstance().getHoles());
-		Collections.sort(holes, new Comparator<Hole>() {
-
-			public int compare(Hole a, Hole b) {
-				return Integer.valueOf(a.getNumber()).compareTo(Integer.valueOf(b.getNumber()));
+		return this.isManaged() ? "valid" : "invalid";
+	}
+	
+	public String parseRestfulUrl() {
+		String viewId =
+			FacesContext.getCurrentInstance().getViewRoot().getViewId();
+		try {
+			String[] segments = viewId.split("/");
+			assert segments.length >= 2;
+			String action = segments[segments.length - 2];
+			String resourceId = segments[segments.length - 1];
+			if (resourceId.contains(".")) {
+				resourceId = resourceId.substring(0, resourceId.lastIndexOf('.'));
 			}
-		});
-
-		return holes;
+			Long courseId = Long.parseLong(resourceId);
+			setCourseId(courseId);
+			return validateEntityFound().equals("valid") ? action : "invalid";
+		} catch (Exception e) {
+			return "invalid";
+		}
 	}
 
 }
