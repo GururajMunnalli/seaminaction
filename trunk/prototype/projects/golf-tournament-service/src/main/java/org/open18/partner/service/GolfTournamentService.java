62bf4f2c6410a7d53d7b725f43f3423b7df0633a
dan.j.allen <dan.j.allen@cc8609e4-7d3f-0410-87dc-e7b0ede3ef86>
2008-04-05 23:10:59 +0000

    restructuring

--- trunk/prototype/projects/golf-tournament-service/src/main/java/org/open18/partner/service/GolfTournamentService.java
+++
@@ -1,9 +0,0 @@
-package org.open18.partner.service;
-
-import org.open18.partner.model.GolfTournament;
-
-import java.util.List;
-
-public interface GolfTournamentService {
-    List<GolfTournament> getFutureTournaments();
-}
