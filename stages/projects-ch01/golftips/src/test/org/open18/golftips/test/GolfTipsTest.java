4b851bc53f52a4a0100ddc458c0581db07e71b2c
dan.j.allen <dan.j.allen@cc8609e4-7d3f-0410-87dc-e7b0ede3ef86>
2008-05-14 03:29:20 +0000

    initial commit for chapter 01 sample code

---
+++ stages/projects-ch01/golftips/src/test/org/open18/golftips/test/GolfTipsTest.java
@@ -0,0 +1,39 @@
+package org.open18.golftips.test;
+
+import org.testng.annotations.Test;
+import org.jboss.seam.mock.SeamTest;
+import javax.faces.application.FacesMessage;
+import java.util.List;
+
+public class GolfTipsTest extends SeamTest {
+
+	@Test
+	public void testAddTip() throws Exception {
+		
+		new NonFacesRequest("/golftips.xhtml") {
+			protected void renderResponse() throws Exception {
+				assert (Boolean) getValue("#{tips.rowCount eq 0}");
+			}
+		}.run();
+		
+		new FacesRequest("/golftips.xhtml") {
+			protected void updateModelValues() throws Exception {
+				setValue("#{tip.author}", "Ben Hogan");
+				setValue("#{tip.category}", "The Swing");
+				setValue("#{tip.content}",
+					"Good golf begins with a good grip.");
+			}
+			
+			protected void invokeApplication() throws Exception {
+				invokeMethod("#{tipAction.add(tip)}");
+			}
+			
+			protected void renderResponse() throws Exception {
+				assert (Boolean) getValue("#{tips.rowCount eq 1}");
+				List<FacesMessage> messages = (List<FacesMessage>) getValue("#{facesMessages.currentMessages}");
+				assert messages.size() == 1;
+				assert "Thanks for the tip, Ben Hogan!".equals(messages.get(0).getSummary());
+			}
+		}.run();
+	}
+}
