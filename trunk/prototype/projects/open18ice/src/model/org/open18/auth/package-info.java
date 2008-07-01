3af21fd9df297c4e3235ce17b8323d316c912d88
dan.j.allen <dan.j.allen@cc8609e4-7d3f-0410-87dc-e7b0ede3ef86>
2008-07-01 21:40:25 +0000

    whitespace

--- trunk/prototype/projects/open18ice/src/model/org/open18/auth/package-info.java
+++ trunk/prototype/projects/open18ice/src/model/org/open18/auth/package-info.java
@@ -1,4 +1,4 @@
-@Namespace(value="http://open18.org/components/auth", prefix="")
+@Namespace(value = "http://open18.org/components/auth", prefix = "")
 @AutoCreate
 package org.open18.auth;
 import org.jboss.seam.annotations.AutoCreate;
