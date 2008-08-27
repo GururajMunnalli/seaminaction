5e3f0391d4dcb5faac0334b894012006e618f351
dan.j.allen <dan.j.allen@cc8609e4-7d3f-0410-87dc-e7b0ede3ef86>
2008-08-27 06:41:49 +0000

    finish adding ant dist

--- trunk/ant.bat
+++ trunk/ant.bat
@@ -8,8 +8,6 @@ if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
 set PROGNAME=ant.bat
 if "%OS%" == "Windows_NT" set PROGNAME=%~nx0%
 
-set SEAM_HOME="%DIRNAME%\opt\jboss-seam-2.0.0.GA"
-
 set ARGS=%ARGS% %*
 
-java -cp "%JAVA_HOME%\lib\tools.jar;%SEAM_HOME%\build\lib\ant-launcher.jar;%SEAM_HOME%\build\lib\ant-nodeps.jar;%SEAM_HOME%\build\lib\ant.jar" -Dant.home="%SEAM_HOME%\lib" org.apache.tools.ant.launch.Launcher -buildfile "%DIRNAME%\build.xml" %ARGS%
+java -cp "%JAVA_HOME%\lib\tools.jar;%DIRNAME%\lib\ant-launcher.jar;%DIRNAME%\lib\ant-nodeps.jar;%DIRNAME%\lib\ant.jar" -Dant.home="%DIRNAME%\lib" org.apache.tools.ant.launch.Launcher -buildfile "%DIRNAME%\build.xml" %ARGS%
