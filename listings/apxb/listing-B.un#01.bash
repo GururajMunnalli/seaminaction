523e1ac25d2e632213251de03e78ee5460d12b94
dan.j.allen <dan.j.allen@cc8609e4-7d3f-0410-87dc-e7b0ede3ef86>
2008-08-27 03:02:57 +0000

    nevermind, not going to put listings in here

--- listings/apxb/listing-B.un#01.bash
+++
@@ -1,2 +0,0 @@
-export JAVA_HOME=/home/twoputt/opt/jdk1.6.0_03
-export PATH=$JAVA_HOME/bin:$PATH
--- listings/ch04/listing-4.un#05.package-info.java
+++
@@ -1,3 +0,0 @@
-@Namespace(value="http://open18.org/components/auth", prefix="")
-package org.open18.auth;
-import org.jboss.seam.annotations.Namespace;
